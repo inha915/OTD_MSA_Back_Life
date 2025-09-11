1979025bf0859a8c1ce6fe76b52c9886e17ed255
iddadda <iddadda241@gmail.com>
2025-09-11 09:20:37 +0900

    복구

---
+++ src/main/java/com/otd/otd_msa_back_life/otd_community/repository/CommunityPostRepository.java
@@ -0,0 +1,7 @@
+package com.otd.otd_msa_back_life.otd_community.repository;
+
+import com.otd.otd_msa_back_life.otd_community.entity.CommunityPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommunityPostRepository extends JpaRepository<CommunityPost, Long> {
+}
