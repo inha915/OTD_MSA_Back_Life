7917e60b744ce74a9382850441fb9fec36d3009f
iddadda <iddadda241@gmail.com>
2025-09-11 09:56:55 +0900

    물섭취 기록 테스트 전

--- src/main/java/com/otd/otd_msa_back_life/water_intake/controller/DailyWaterIntakeController.java
+++ src/main/java/com/otd/otd_msa_back_life/water_intake/controller/DailyWaterIntakeController.java
@@ -1,9 +1,13 @@
 package com.otd.otd_msa_back_life.water_intake.controller;
 
 
+import com.otd.otd_msa_back_life.water_intake.model.DailyWaterIntakePostReq;
 import com.otd.otd_msa_back_life.water_intake.service.DailyWaterIntakeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class DailyWaterIntakeController {
     private final DailyWaterIntakeService DailyWaterIntakeService;
+
+    @PostMapping
+    public ResponseEntity<?> saveDailyWaterIntake(@RequestBody DailyWaterIntakePostReq req){
+        log.info("req:{}", req);
+        Long result = DailyWaterIntakeService.saveDailyWaterIntake(req);
+        log.info("result:{}", result);
+        return ResponseEntity.ok(result);
+    }
 }
--- src/main/java/com/otd/otd_msa_back_life/water_intake/service/DailyWaterIntakeService.java
+++ src/main/java/com/otd/otd_msa_back_life/water_intake/service/DailyWaterIntakeService.java
@@ -1,6 +1,8 @@
 package com.otd.otd_msa_back_life.water_intake.service;
 
 
+import com.otd.otd_msa_back_life.water_intake.entity.DailyWaterIntake;
+import com.otd.otd_msa_back_life.water_intake.model.DailyWaterIntakePostReq;
 import com.otd.otd_msa_back_life.water_intake.repository.DailyWaterIntakeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,5 +12,13 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class DailyWaterIntakeService {
-    private final DailyWaterIntakeRepository DailyWaterIntakeRepository;
+    private final DailyWaterIntakeRepository dailyWaterIntakeRepository;
+
+    public Long saveDailyWaterIntake(DailyWaterIntakePostReq req) {
+        DailyWaterIntake dailyWaterIntake = DailyWaterIntake.builder()
+                .amountLiter(req.getAmountLiter())
+                .build();
+        dailyWaterIntakeRepository.save(dailyWaterIntake);
+        return dailyWaterIntake.getDailyWaterIntakeId();
+    }
 }
