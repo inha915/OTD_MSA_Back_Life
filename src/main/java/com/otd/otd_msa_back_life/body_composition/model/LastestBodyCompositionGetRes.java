f4b07c0b2d060d0e0e7e641e1511df526e41cb7c
iddadda <iddadda241@gmail.com>
2025-10-08 17:45:36 +0900

    필드명 수정

--- src/main/java/com/otd/otd_msa_back_life/body_composition/model/LastestBodyCompositionGetRes.java
+++ src/main/java/com/otd/otd_msa_back_life/body_composition/model/LastestBodyCompositionGetRes.java
@@ -1,5 +1,7 @@
 package com.otd.otd_msa_back_life.body_composition.model;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -9,6 +11,7 @@ import java.time.LocalDateTime;
 @Getter
 @Builder
 @ToString
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class LastestBodyCompositionGetRes {
     private Long measuredId;
     private Long userId;
