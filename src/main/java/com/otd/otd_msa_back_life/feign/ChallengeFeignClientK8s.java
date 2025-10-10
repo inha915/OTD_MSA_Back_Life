075db9252430ae01a47eeec444daedca08e28822
greenhc502 <greenhc502@gmail.com>
2025-10-10 19:08:48 +0900

    sever to server  통신 수정중

--- src/main/java/com/otd/otd_msa_back_life/feign/ChallengeFeignClientK8s.java
+++
@@ -1,9 +0,0 @@
-package com.otd.otd_msa_back_life.feign;
-
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Profile;
-
-@Profile("prod")
-@FeignClient(name = "${constants.feign-client.challenge.name}"    )
-public interface ChallengeFeignClientK8s extends ChallengeFeignClient {
-}
