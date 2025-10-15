3b0e78b17c55f2a59dff50dfc29297ac84c6cc1c
d-kjh <dkjh9942@gmail.com>
2025-10-15 12:56:10 +0900

    10/15 feign localDate 수정

--- src/main/java/com/otd/otd_msa_back_life/feign/model/ChallengeRecordDeleteReq.java
+++ src/main/java/com/otd/otd_msa_back_life/feign/model/ChallengeRecordDeleteReq.java
@@ -1,5 +1,6 @@
 package com.otd.otd_msa_back_life.feign.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -14,8 +15,13 @@ public class ChallengeRecordDeleteReq {
     private Long userId;
     private String name;
     private Long recordId;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate recordDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate today;
+
     private int count;
     private int totalKcal;
 }
--- src/main/java/com/otd/otd_msa_back_life/feign/model/MealDataReq.java
+++ src/main/java/com/otd/otd_msa_back_life/feign/model/MealDataReq.java
@@ -1,5 +1,6 @@
 package com.otd.otd_msa_back_life.feign.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -11,8 +12,13 @@ import java.time.LocalDate;
 @Builder
 public class MealDataReq {
     private Long userId;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate recDate;
+
     private Double value;
     private String name;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate today;
 }
